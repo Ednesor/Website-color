d48fe6b8c4a3e8930914ddc9db64a983104a6947
Ednesor <edgar.funes2001@gmail.com>
2023-09-07 20:34:49 -0300

    feat: create guide

--- src/app/guide/page.tsx
+++ src/app/guide/page.tsx
@@ -1,7 +1,46 @@
-import React from 'react'
+"use client";
+import React from "react";
+import data from "../../utils/languaje.json";
+import { useAppSelector } from "@/redux/hooks";
+import style from "./guide.module.css";
+import Link from "next/link";
 
 export default function page() {
+  const lang = useAppSelector((state) => state.configReducer.lang);
+
   return (
-    <div>Esto es la <h1>guiia</h1></div>
-  )
+    <main className={style.main}>
+      <Link className={style.button} href={"/home"}>
+        {data[lang][1]}
+      </Link>
+      <h1>{data[lang][3]}</h1>
+      <p>{data[lang][4]}</p>
+
+      <h4>{data[lang][5]}</h4>
+      <p>{data[lang][6]}</p>
+      <p>{data[lang][7]}</p>
+      <h3>{data[lang][8]}</h3>
+      
+      <ul>
+        <li>
+          <h4>{data[lang][9]}</h4>
+          <ul>
+            <li>{data[lang][10]}</li>
+          </ul>
+        </li>
+        <li>
+          <h4>{data[lang][11]}</h4>
+          <ul>
+            <li>{data[lang][12]}</li>
+          </ul>
+        </li>
+        <li>
+          <h4>{data[lang][13]}</h4>
+          <ul>
+            <li>{data[lang][14]}</li>
+          </ul>
+        </li>
+      </ul>
+    </main>
+  );
 }
